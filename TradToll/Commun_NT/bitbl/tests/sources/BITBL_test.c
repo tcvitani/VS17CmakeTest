b665c7b91b225e96df9a1cf58eac4ef6591226cb
tihomir <tihomir.cvitanic@emovis.hr>
2025-11-24 17:37:55 +0100

    ..

--- TradToll/Commun_NT/bitbl/tests/sources/BITBL_test.c
+++ TradToll/Commun_NT/bitbl/tests/sources/BITBL_test.c
@@ -32,15 +32,29 @@
 
 
 
-PROTECTED int __cdecl main(
-    IN int iArgc,
-    IN char ** ppcArgc )
+PROTECTED int __cdecl main(IN int iArgc,  IN char ** ppcArgc )
 {
     int iErr = NO_ERROR;
-    
+	DWORD dwVal = 0;
+	unsigned char pSrc[] = { 0x15, 0xA8, 0x2D, 0xB9 };
+
+	DWORD dwCurrentBitPos = 8;
+	DWORD dwAvailableBits = 24;
+
     __try
     {
         // Insert test code here
+		dwVal = 0; 
+		if (BITBLGetNumber(&dwVal, 8, pSrc, &dwCurrentBitPos, &dwAvailableBits))
+		{
+			printf("BITBLGetNumber returned TRUE: Val:%X", dwVal);
+		}
+		else
+		{
+			printf("BITBLGetNumber returned FALSE: ");
+
+		}
+
         
     }
     __finally
@@ -48,5 +62,5 @@ PROTECTED int __cdecl main(
 		;
     }
 
-	return iErr;
+ 	return iErr;
 }
